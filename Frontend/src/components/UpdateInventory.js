a304078746ce9ceafe239e74a2935c3e44800825
Rivin Jayawardana <127378095+RivinJayawardhana@users.noreply.github.com>
2024-09-11 11:56:19 +0530

    date type

--- Frontend/src/components/UpdateInventory.js
+++ Frontend/src/components/UpdateInventory.js
@@ -165,6 +165,27 @@ export default function Updateinventory({
                                 );
                               })}
                             </select>
+
+
+                         
+                          </div>
+
+                          <div>
+                            <label
+                              htmlFor="stockSold"
+                              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                              Assigned Date
+                            </label>
+                            <input
+                              type="date"
+                              name="date"
+                              id="date"
+                             
+                              required onChange={(e) => setBrand(e.target.value)} 
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                              placeholder="date"
+                            />
                           </div>
                           <div>
                             <label
