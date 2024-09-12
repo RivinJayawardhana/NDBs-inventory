b29289769699cf6967d8e4de89eb2880f647f57d
Rivin Jayawardana <127378095+RivinJayawardhana@users.noreply.github.com>
2024-09-12 08:30:01 +0530

    date remove

--- Frontend/src/components/AddInventories.js
+++ Frontend/src/components/AddInventories.js
@@ -140,25 +140,7 @@ export default function AddInventories({
                             </select>
                           </div>
 
-                          {user.toLocaleLowerCase() !== 'none' && (
-                          <div>
-                            <label
-                              htmlFor="stockSold"
-                              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                            >
-                              Assign Date
-                            </label>
-                            <input
-                              type="date"
-                              name="date"
-                              id="date"
-                             
-                              required onChange={(e) => setdate(e.target.value)} 
-                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                              placeholder="date"
-                            />
-                          </div>
-                            )}
+                        
                           <div>
                             <label
                               htmlFor="stockSold"
