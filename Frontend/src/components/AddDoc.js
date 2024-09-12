10bdb579d640248863eb2cbb941682d2902b5fdf
Rivin Jayawardana <127378095+RivinJayawardhana@users.noreply.github.com>
2024-09-12 11:34:38 +0530

    n1

--- Frontend/src/components/AddDoc.js
+++ Frontend/src/components/AddDoc.js
@@ -220,7 +220,7 @@ export default function Adddoc({
                               htmlFor="stockSold"
                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                             >
-                             Email
+                             Doc type
                             </label>
                             <input
                               type="email"
@@ -231,6 +231,24 @@ export default function Adddoc({
                               placeholder="Email"
                             />
                           </div>
+
+                          <div>
+                            <label
+                              htmlFor="stockSold"
+                              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                            Remark
+                            </label>
+                            <input
+                              type="email"
+                              name="stockSold"
+                              id="stockSold"
+                              required onChange={(e) => setemail(e.target.value)}
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                              placeholder="Email"
+                            />
+                          </div>
+
 
                           
                         
