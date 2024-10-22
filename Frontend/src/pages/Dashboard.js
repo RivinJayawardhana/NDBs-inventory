b0c98c2a574f817306829d5d050127152e2f027f
Rivin Jayawardana <127378095+RivinJayawardhana@users.noreply.github.com>
2024-10-22 16:36:37 +0530

    ..

--- Frontend/src/pages/Dashboard.js
+++ Frontend/src/pages/Dashboard.js
@@ -156,7 +156,7 @@ function Dashboard() {
         <article className="flex flex-col gap-4 rounded-lg border  border-gray-100 bg-white p-6  ">
           <div className="inline-flex gap-2 self-end rounded bg-green-100 p-1 text-green-600">
          
-          ..
+        
             <span className="text-xs font-medium"> Finance Department </span>
           </div>
 
