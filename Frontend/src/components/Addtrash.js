ba092e903f53f0b91017ebade03d370fe8796c28
Rivin Jayawardana <127378095+RivinJayawardhana@users.noreply.github.com>
2024-11-18 15:53:23 +0530

    assertid up

--- Frontend/src/components/Addtrash.js
+++ Frontend/src/components/Addtrash.js
@@ -146,7 +146,7 @@ export default function Addtrash({
                               id="stockSold"
                               required onChange={(e) => setAssertName(e.target.value)}
                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                              placeholder="Assert Name"
+                              placeholder="Assert ID"
                             />
                           </div>
                        
